ca4d13f17694f9080b7ff3cd2afc1ca1200d26dd
Chris Allmon <chrisa.code@gmail.com>
2023-09-27 15:58:59 -0500

    transition work

--- src/components/MormalImageSlider.jsx
+++ src/components/MormalImageSlider.jsx
@@ -5,6 +5,7 @@ function ImageSlider() {
   const [mouseCurrentX, setMouseCurrentX] = useState(0);
   const [prevPercentage, setPrevPercentage] = useState(0);
   const [percentage, setPercentage] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
   const track = useRef(null);
 
   const handleOnDown = (e) => {
@@ -24,6 +25,8 @@ function ImageSlider() {
       setPrevPercentage(percentage);
       return;
     }
+
+    console.log(isDragging);
     const mouseDelta = mouseDownAt - mouseCurrentX;
     const maxDelta = track.current.scrollWidth;
     // const maxDelta = window.innerWidth;
@@ -45,7 +48,7 @@ function ImageSlider() {
     );
 
     [...track.current.children].map((image) => {
-      image.animate(
+      image.children[0].animate(
         {
           objectPosition: `${100 + nextPercentage}% center`,
         },
@@ -88,12 +91,17 @@ function ImageSlider() {
   const pointer = useRef({ x: 0, y: 0 });
 
   const imageMouseDown = (e) => {
+    setIsDragging(true);
     pointer.current = { x: e.clientX, y: e.clientY };
   };
-  const imageMouseUp = (e, path) => {
+  const imageMouseUp = (e) => {
     const { x, y } = pointer.current;
     if (Math.abs(e.clientX - x) < 10 && Math.abs(e.clientY - y) < 10) {
-      window.location.href = path;
+      setIsDragging(false);
+    } else {
+      setTimeout(() => {
+        setIsDragging(false);
+      }, 250);
     }
   };
 
@@ -104,47 +112,74 @@ function ImageSlider() {
       ref={track}
       data-mouse-down-at={mouseDownAt}
       data-prev-percentage={prevPercentage}
+      onMouseDown={imageMouseDown}
+      onMouseUp={imageMouseUp}
     >
-      <img
-        id="solv-img"
-        className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+      <a
+        href="/clients/solv"
+        onClick={(event) => (isDragging ? event.preventDefault() : null)}
         draggable="false"
-        onMouseDown={(e) => imageMouseDown(e)}
-        onMouseUp={(e) => imageMouseUp(e, "/clients/solv")}
-        src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
-      />
-      <img
-        id="lobby-img"
-        className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+        className=" select-none"
+      >
+        <img
+          transition:name="solv-img"
+          className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+          draggable="false"
+          src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
+        />
+      </a>
+      <a
+        href="/clients/lobby"
+        onClick={(event) => (isDragging ? event.preventDefault() : null)}
         draggable="false"
-        onMouseDown={(e) => imageMouseDown(e)}
-        onMouseUp={(e) => imageMouseUp(e, "/clients/lobby")}
-        src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
-      />
-      <img
-        id="simplifyy-img"
-        className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+        className=" select-none"
+      >
+        <img
+          transition:name="lobby-img"
+          className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+          draggable="false"
+          src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
+        />
+      </a>
+      <a
+        href="/clients/simplifyy"
+        onClick={(event) => (isDragging ? event.preventDefault() : null)}
         draggable="false"
-        onMouseDown={(e) => imageMouseDown(e)}
-        onMouseUp={(e) => imageMouseUp(e, "/clients/simplifyy")}
-        src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2273&q=80"
-      />
-      <img
-        id="civic-img"
-        className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+        className=" select-none"
+      >
+        <img
+          transition:name="simplifyy-img"
+          className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+          draggable="false"
+          src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2273&q=80"
+        />
+      </a>
+      <a
+        href="/clients/civicplus"
+        onClick={(event) => (isDragging ? event.preventDefault() : null)}
         draggable="false"
-        onMouseDown={(e) => imageMouseDown(e)}
-        onMouseUp={(e) => imageMouseUp(e, "/clients/civicplus")}
-        src="https://images.unsplash.com/photo-1548946061-4af3de8b577c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2503&q=80"
-      />
-      <img
-        id="ge-img"
-        className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+        className=" select-none"
+      >
+        <img
+          transition:name="civic-img"
+          className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+          draggable="false"
+          src="https://images.unsplash.com/photo-1548946061-4af3de8b577c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2503&q=80"
+        />
+      </a>
+      <a
+        href="/clients/ge"
+        onClick={(event) => (isDragging ? event.preventDefault() : null)}
         draggable="false"
-        onMouseDown={(e) => imageMouseDown(e)}
-        onMouseUp={(e) => imageMouseUp(e, "/clients/ge")}
-        src="https://images.unsplash.com/photo-1503379230423-19c53f7e9a33?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2346&q=80"
-      />
+        className=" select-none"
+      >
+        <img
+          transition:name="ge-img"
+          className="slide-image w-[40vmin] h-[56vmin] object-cover object-right select-none"
+          draggable="false"
+          src="https://images.unsplash.com/photo-1503379230423-19c53f7e9a33?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2346&q=80"
+        />
+      </a>
     </div>
   );
 }
