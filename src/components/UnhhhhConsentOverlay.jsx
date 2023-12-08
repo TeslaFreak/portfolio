bd8fb50a85dad37a212318bae0437c328ce133b9
Chris Allmon <chrisa.code@gmail.com>
2023-12-07 20:40:54 -0600

    fixed autoplay

---
+++ src/components/UnhhhhConsentOverlay.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { MdArrowDownward } from "react-icons/md";
+
+const ConsentOverlay = ({ setHasConsent, setAutoPlay }) => {
+  const handleConsent = () => {
+    setHasConsent(true);
+    setAutoPlay(true);
+  };
+
+  const handleDeny = () => {
+    setHasConsent(false);
+    setAutoPlay(false);
+  };
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-75 flex justify-center items-center z-10">
+      <div className="flex flex-col items-center p-4 bg-gray-800 ">
+        <div className="text-white text-xl mb-4 text-center">
+          This site contains automated audio playback. We would love your
+          consent before we play anything for you. Alternatively, just click any
+          text bubble to play them individually!
+        </div>
+        <div>
+          <button
+            onClick={handleConsent}
+            className="px-4 py-2 bg-green-500 text-white rounded-md mr-2"
+          >
+            Start Playing!
+          </button>
+          <button
+            onClick={handleDeny}
+            className="px-4 py-2 bg-red-500 text-white rounded-md"
+          >
+            Let Me Play Them At My Own Pace.
+          </button>
+        </div>
+      </div>
+      {/* Separate text blurb block */}
+      <div className="absolute bottom-16 right-4 p-4 bg-gray-800 rounded-md w-80 items-end flex flex-col">
+        <div className="text-white text-sm text-center">
+          If you dont want to start playing now, you can always turn AutoPlay
+          back on at any time to listen to the full conversation from any point
+          you choose.
+        </div>
+
+        <MdArrowDownward className="text-white text-4xl animate-bounce" />
+      </div>
+    </div>
+  );
+};
+
+export default ConsentOverlay;
--- src/components/UnhhhhInterface.jsx
+++ src/components/UnhhhhInterface.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import LoadingOverlay from "./UnhhhhLoadingOverlay";
+import { MdAutorenew, MdCancel, MdShare } from "react-icons/md";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ConsentOverlay from "./UnhhhhConsentOverlay";
 
 const UnhhhhInterface = () => {
   const [messages, setMessages] = useState([]);
-
+  const [audioList, setAudioList] = useState([]);
+  const [hasConsent, setHasConsent] = useState(undefined);
   const [topic, setTopic] = useState("");
   const [status, setStatus] = useState("IDLE");
+  const autoPlayRef = useRef(false);
+  const [autoPlay, setAutoPlay] = useState(false);
+  const [isPlayiing, setIsPlaying] = useState(false);
+  const currentlyPlayingAudio = useRef(null);
   const [jobId, setJobId] = useState("");
+
+  useEffect(() => {
+    let params = new URL(document.location).searchParams;
+    let name = params.has("episode");
+    if (params.has("episode")) {
+      pollEpisodeStatus(params.get("episode"));
+    }
+  }, []);
+
+  useEffect(() => {
+    autoPlayRef.current = autoPlay;
+  }, [autoPlay]);
+
+  useEffect(() => {
+    if (hasConsent && autoPlay && currentlyPlayingAudio.current === null) {
+      playAudioList(0);
+    }
+  }, [audioList, hasConsent, autoPlay, isPlayiing]);
 
   const episodeEndpoint =
     "https://66j5f8jkuc.execute-api.us-east-1.amazonaws.com/staging/episode";
@@ -20,7 +47,14 @@ const UnhhhhInterface = () => {
         body: JSON.stringify({ topic }),
       });
       const data = await response.json();
+      setStatus("QUEUED");
       pollEpisodeStatus(data.executionArn);
+
+      let searchParams = new URLSearchParams(window.location.search);
+      searchParams.set("episode", encodeURIComponent(data.executionArn));
+      let newRelativePathQuery =
+        window.location.pathname + "?" + searchParams.toString();
+      history.pushState(null, "", newRelativePathQuery);
     } catch (error) {
       setStatus(error);
       console.error("Error calling first endpoint:", error);
@@ -48,7 +82,7 @@ const UnhhhhInterface = () => {
       if (data.status && data.status === "SUCCEEDED") {
         setJobId(data.job_id);
         setMessages(data.script_array);
-        playAudioList(0, data.presignedUrls);
+        setAudioList(data.presignedUrls);
       } else {
         setTimeout(() => pollEpisodeStatus(arn), 7000); // Poll every 7 seconds
       }
@@ -58,33 +92,64 @@ const UnhhhhInterface = () => {
     }
   };
 
-  const playAudioList = (index = 0, audioList) => {
+  const playAudioList = (index = 0) => {
+    console.log(`index ${index} called`);
+    clearTimeouts();
     if (index < audioList.length) {
-      var audio = new Audio(audioList[index]);
+      if (currentlyPlayingAudio.current !== null) {
+        try {
+          currentlyPlayingAudio.current.pause();
+        } catch {
+          console.log(
+            "tried to pause audio, but there is no audio currently playing"
+          );
+        }
+      }
+      let audio = new Audio(audioList[index]);
+      currentlyPlayingAudio.current = audio;
       audio.addEventListener(
         "loadedmetadata",
         function () {
+          console.log(`index ${index} playing`);
           audio.play();
-          setTimeout(
-            () => playAudioList(index + 1, audioList),
-            audio.duration * 1000
-          ); // Wait the length of the track before playing the next
+          if (!!autoPlayRef.current) {
+            setTimeout(() => {
+              if (!!autoPlayRef.current) {
+                playAudioList(index + 1);
+              }
+            }, audio.duration * 1000); // Wait the length of the track before playing the next
+          }
         },
         false
       );
     }
   };
 
+  const clearTimeouts = () => {
+    const highestId = window.setTimeout(() => {
+      for (let i = highestId; i >= 0; i--) {
+        window.clearInterval(i);
+      }
+    }, 0);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     createEpisodeJob();
+  };
 
-    // if (message.trim() !== "") {
-    //   // Toggle the speaker for each message to differentiate between the two
-    //   const speaker = messages.length % 2 === 0 ? "left" : "right";
-    //   setMessages([...messages, { text: message, speaker }]);
-    //   setMessage("");
-    // }
+  const handleShare = () => {
+    navigator.clipboard.writeText(window.location.href);
+    toast("📋 Link copied to clipboard", {
+      position: "bottom-right",
+      autoClose: 1500,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "colored",
+    });
   };
 
   return (
@@ -97,29 +162,75 @@ const UnhhhhInterface = () => {
             value={topic}
             onChange={(e) => setTopic(e.target.value)}
             placeholder="Enter the episode topic..."
-            className="flex-grow p-2 border rounded-md"
+            className={`flex-grow p-2 border rounded-md ${
+              !["IDLE", "SUCCEEDED"].includes(status)
+                ? "bg-gray-200 cursor-not-allowed"
+                : ""
+            }`}
+            disabled={!["IDLE", "SUCCEEDED"].includes(status)}
           />
           <button
-            className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+            className={`ml-2 px-4 py-2 text-white rounded-md ${
+              !["IDLE", "SUCCEEDED"].includes(status)
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-blue-500"
+            }`}
             type="submit"
+            disabled={!["IDLE", "SUCCEEDED"].includes(status)}
           >
             Submit
           </button>
+          <button
+            className={`ml-2 px-4 py-2 text-white rounded-md flex items-center ${
+              ["IDLE"].includes(status)
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-green-500"
+            }`}
+            onClick={handleShare}
+            type="button"
+            disabled={["IDLE"].includes(status)}
+          >
+            <MdShare className="mr-2" />
+            Share
+          </button>
         </form>
+        <div className="fixed bottom-4 right-4">
+          <button
+            className={`flex items-center px-4 py-2 text-white rounded-md ${
+              autoPlay ? "bg-purple-500" : "bg-gray-500"
+            }`}
+            onClick={() => setAutoPlay(!autoPlay)}
+          >
+            {autoPlay ? (
+              <MdAutorenew className="mr-2" />
+            ) : (
+              <MdCancel className="mr-2" />
+            )}
+            AutoPlay
+          </button>
+        </div>
       </div>
       {!["IDLE", "SUCCEEDED"].includes(status) && (
         <LoadingOverlay status={status} />
       )}
+      {["IDLE", "SUCCEEDED"].includes(status) && hasConsent === undefined && (
+        <ConsentOverlay
+          setHasConsent={setHasConsent}
+          setAutoPlay={setAutoPlay}
+        />
+      )}
+      <ToastContainer />
       {/* Chat interface */}
       <div className="p-4 mt-16 max-w-lg mx-auto space-y-4">
         {messages.map((msg, idx) => (
           <div
             key={idx}
-            className={`my-2 p-3 rounded-md max-w-xs ${
+            className={`my-2 p-3 rounded-md max-w-xs transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg ${
               msg.speaker.toLowerCase() === "trixie"
                 ? "bg-blue-100 text-blue-800"
                 : "bg-green-100 text-green-800 ml-auto"
             }`}
+            onClick={() => playAudioList(idx)}
           >
             {msg.line}
           </div>
